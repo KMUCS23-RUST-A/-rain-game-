1cb1229367d232eb892ad39da83d1daa42fed460
ChangHoon Sung <changhoon.dev@gmail.com>
2023-04-29 16:31:01 +0900

    Add the comments

--- examples/test-play-thread.rs
+++ examples/test-play-thread.rs
@@ -12,21 +12,24 @@ const WIDTH: i32 = 80;
 
 #[tokio::main]
 async fn main() {
+    // 서버 TCP 연결
     let mut socket = tokio::net::TcpStream::connect("127.0.0.1:12345")
         .await
         .unwrap();
 
-    // create channel
+    // 채널 생성
     let (mgr_writer, mut mgr_reader) = mpsc::channel(10);
     let (game_writer, mut game_reader) = mpsc::channel(10);
 
-    // create a manager task
+    // GameManager 쓰레드
     tokio::spawn(async move {
         let mut buf = [0; 1];
 
+        // I/O Multiplexing
         tokio::select! {
-            // got message from socket
-            srv_msg = socket.read(&mut buf) => {
+
+            // 서버에서 메세지 1개(read_exact) 읽기
+            srv_msg = socket.read_exact(&mut buf) => {
                 let srv_msg = Message::from(srv_msg.unwrap() as u8);
                 println!("GOT SERVER MESSAGE: {:?}", srv_msg);
                 match srv_msg {
@@ -46,21 +49,21 @@ async fn main() {
                 game_writer.send(srv_msg).await.unwrap();   // 게임에게 서버 메세지 전달
             }
 
-            // got message from game
+            // 게임 메세지 1개 읽기
             msg = game_reader.recv() => {
                 println!("GOT GAME MESSAGE: {:?}", msg);
                 let msg = msg.unwrap();
                 match msg {
                     Message::Attacked | Message::GameOver => {
                         socket.write_all(&[msg as u8]).await.unwrap();  // 서버에게 게임 메세지 전달
-                        }
+                    }
                     _ => {} // 위 메세지 외에는 무시
                 }
             }
         }
     });
 
-    // create a game task
+    // Ncurses 게임 실행 쓰레드
     tokio::spawn(async move {
         // run game
         initscr();
@@ -73,6 +76,7 @@ async fn main() {
         let line = "-".repeat(WIDTH as usize);
 
         loop {
+            // GameManager로부터 메세지 non-blocking으로 받기
             match mgr_reader.try_recv() {
                 Ok(msg) => match msg {
                     Message::GameOver => {
@@ -82,10 +86,10 @@ async fn main() {
                     Message::Attacked => {
                         println!("GOT MANAGER MESSAGE: {:?}", msg);
                     }
-                    _ => {} // ignore Message::GameStart and Message::Waiting
+                    _ => {} // 위 메세지 외에는 무시
                 },
-                Err(TryRecvError::Empty) => {} // no message from manager
-                Err(TryRecvError::Disconnected) => break, // manager closed
+                Err(TryRecvError::Empty) => {} // 읽을 메세지 없음
+                Err(TryRecvError::Disconnected) => break, // GameManager 닫힘
             }
 
             let input = getch();
@@ -103,6 +107,7 @@ async fn main() {
             } else {
                 None
             };
+
             let word_completed = game.update(input_char);
             if word_completed {
                 mgr_writer.send(Message::Attacked).await.unwrap();  // GameManager에게 게임 메세지 전달
