186b70d4c15ba7d7ed285f1e1f1482570e77c36f
ChangHoon Sung <changhoon.dev@gmail.com>
2023-04-29 23:04:46 +0900

    Revert "공격 단어, 단어 상태, 체력 구현에 대한 설명"

--- src/game/game_state.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Copy, Clone, PartialEq)]
-pub enum GameState {
-    StartGame = 0,
-    CompleteWord = 1,
-    CompleteAttackWord = 2,
-    Lose = 3,
-    InProgress = 4,
-}
--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,4 +1,3 @@
 pub mod game;
-pub mod game_state;
 mod vocab;
 mod word;
